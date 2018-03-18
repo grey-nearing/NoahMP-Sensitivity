5351a6ba06f9c2b0b2f9293c201507733136bef5
greyNearing <grey.nearing@gmail.com>
2018-03-18 15:52:01 -0400

    cleaned up model source directory and added a makefile

--- environment_setup.sh
+++ environment_setup.sh
@@ -3,6 +3,7 @@
 # modules
 module purge
 module load comp/intel-16.0.3.210
+module load other/git
 
 # linking
 export LD_LIBRARY_PATH=$LD_LIBRARY_PATH:/discover/nobackup/projects/lis/libs/netcdf/4.3.3.1_intel-14.0.3.174_sp3/lib
