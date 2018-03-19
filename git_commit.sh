5eb5fa0626974616b9d780fd8126c85d0060522e
greyNearing <grey.nearing@gmail.com>
2018-03-19 01:05:50 -0400

    added the git_commit script necessary for pushing results files

---
+++ git_commit.sh
@@ -0,0 +1,21 @@
+#!/bin/bash -x
+
+minimumsize=99000000
+
+for f in $(find . -type f)
+do 
+
+ actualsize=$(wc -c <"$f")
+ echo $f $actualsize
+
+ if [ "$actualsize" -lt "$minimumsize" ] 
+  then 
+#    echo 'small enough'
+
+ env -i git add $f
+ env -i git commit -m "$f"
+ git push -u origin master
+
+ fi
+done
+
